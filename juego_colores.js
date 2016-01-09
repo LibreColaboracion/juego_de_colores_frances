db837575f78e39920082bf17c0318c85db8bccfc
jhonedison <jhonedisongl@gmail.com>
2016-01-09 18:22:06 -0500

    Subo todos los códigos hechos para el juego de colores

---
+++ juego_colores.js
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+  function cambio1 (){
+	  var start = document.getElementById('b1');
+	  var marco = document.getElementById('mar');
+        	marco.className=("margen1");
+        }
+  
+  function cambio2 (){
+	  var verdadera2 = document.getElementById('b2');
+	  var marco = document.getElementById('mar');
+        	marco.className=("margen2");
+        
+  }
+  
+  function cambio3 (){
+	  var verdadera3 = document.getElementById('b4');
+	  var marco = document.getElementById('mar');
+        	marco.className=("margen3");
+        
+  }
+  
+  function cambio4 (){
+	  var verdadera4 = document.getElementById('b7');
+	  var marco = document.getElementById('mar');
+        	marco.className=("margen4");
+        
+  }
+  
+  function cambio5 (){
+	  var verdadera5 = document.getElementById('b8');
+	  var marco = document.getElementById('mar');
+        	marco.className=("margen5");
+        
+  }
+  
+  function cambio6 (){
+	  var verdadera6 = document.getElementById('b10');
+	  var marco = document.getElementById('mar');
+        	marco.className=("margen6");
+        
+  }
+  
+  function cambio7 (){
+	  var verdadera7 = document.getElementById('b13');
+	  var marco = document.getElementById('mar');
+        	marco.className=("margen7");
+        
+  }
+  
+  function cambio8 (){
+	  var verdadera8 = document.getElementById('b14');
+	  var marco = document.getElementById('mar');
+        	marco.className=("margen8");
+        
+  }
+  
+  function cambio9 (){
+	  var verdadera9 = document.getElementById('b17');
+	  var marco = document.getElementById('mar');
+        	marco.className=("margen9");
+        
+  }
+  
+  function cambio10 (){
+	  var verdadera10 = document.getElementById('b19');
+	  var marco = document.getElementById('mar');
+        	marco.className=("margen10");
+        
+  }
+  
+  function cambio11 (){
+	  var verdadera11 = document.getElementById('b21');
+	  var marco = document.getElementById('mar');
+        	marco.className=("margen11");
+        
+  }
